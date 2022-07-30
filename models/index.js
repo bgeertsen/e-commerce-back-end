2271b0a96d28e97ade3590d8d0eed285827b7955
bg <62928419+bgeertsen@users.noreply.github.com>
2022-07-30 15:17:19 -0700

    finished product-routes

--- models/index.js
+++ models/index.js
@@ -11,16 +11,30 @@ Product.belongsTo(Category, {
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'product_id'
+  foreignKey: 'category_id'
 })
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: ''
+  as: 'tags',
+  foreignKey: 'product_id'
 })
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tags',
+  foreignKey: 'tag_id'
+})
+
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id'
+});
+
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
